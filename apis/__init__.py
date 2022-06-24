822678fa21ff7b6111f7aaa22cc8ba227245d541
Nick <nick@tfsi.dev>
2022-06-24 02:17:50 +0100

    added function on startup that checks for required files

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter
 from .todo import router as todo_router
 from .auth import router as auth_router
+from core.fileops import FileOps
+import os
+from pathlib import Path
+from data import Config
+from core.startup import StartupChecks
 
 router = APIRouter()
+
+
+@router.on_event("startup")
+def startup():
+    StartupChecks.startup_checks()
+
 router.include_router(todo_router, prefix="/todo", tags=["todo"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
---
+++ core/startup.py
@@ -0,0 +1,96 @@
+import base64
+import os
+from data import Config
+import json
+from cryptography.fernet import Fernet
+import codecs
+
+class StartupChecks(object):
+
+    key = base64.urlsafe_b64encode(Config.JSON_SECRET_KEY.encode())
+    fernet = Fernet(key)
+    reader = codecs.getreader("utf-8")
+
+    def startup_checks():
+        print("Performing startup checks...")
+        files = []
+        for filename in os.listdir("./data/"):
+            if filename.endswith(".json"):
+                files.append(filename)
+        print("Encryption enabled...") if Config.ENCRYPT_JSON else print("Encryption disabled.")
+        if "users.json" in files:
+            print("users.json exists...")
+            with open("./data/users.json", "rb") as f:
+                try:
+                    if Config.ENCRYPT_JSON:
+                        data = f.read().decode()
+                        data = json.loads(data)["encrypted"]
+                        StartupChecks.fernet.decrypt(data.encode()).decode()
+                    if not Config.ENCRYPT_JSON:
+                        json.load(f) 
+                    print("users.json loaded successfully...")
+                except (json.decoder.JSONDecodeError, KeyError):
+                    f.close()
+                    print("users.json is not valid JSON...")
+                    print("Moving users.json to users.json.bak...")
+                    os.rename("./data/users.json", "./data/users.json.bak")
+                    print("Creating new users.json...")
+                    with open("./data/users.json", "wb") as f:
+                        f.write("{}".encode())
+                    if Config.ENCRYPT_JSON:
+                        print("Encrypting new users.json...")
+                        json_file = json.load(f)
+                        encrypted = json.dumps(json_file).encode()
+                        encrypted = StartupChecks.fernet.encrypt(encrypted).decode()
+                        write = {"encrypted": encrypted}
+                        f.seek(0)
+                        f.write(json.dumps(write, indent=4).encode())
+                        f.truncate()
+                        f.close()
+
+        elif "users.json" not in files:
+            print("users.json does not exist, creating new one...")
+            with open("./data/users.json", "w") as f:
+                json.dump({}, f)
+            if Config.ENCRYPT_JSON:
+                print("Encrypting new users.json...")
+                with open("./data/users.json", "rb+") as f:
+                    data = json.load(f)
+                    encrypted = json.dumps(data).encode()
+                    encrypted = StartupChecks.fernet.encrypt(encrypted).decode()
+                    write = {"encrypted": encrypted}
+                    f.seek(0)
+                    f.write(json.dumps(write, indent=4).encode())
+                    f.truncate()
+                    f.close()
+        with open("./data/users.json", "rb") as f:
+            if Config.ENCRYPT_JSON:
+                data = json.load(f)
+                encrypted = data["encrypted"]
+                encrypted = StartupChecks.fernet.decrypt(encrypted.encode()).decode()
+                data = json.loads(encrypted)
+            else:
+                data = json.load(f)
+        if data:
+            print("Looping through user IDs...")
+        else:
+            print("No users found...")
+        for user in data:
+            id = data[user]['id']
+            if f"{id}.json" not in files:
+                print("JSON file for user ID {} does not exist, creating new one...".format(id))
+                with open(f"./data/{id}.json", "w") as f:
+                    json.dump({}, f)
+                if Config.ENCRYPT_JSON:
+                    print(f"Encrypting JSON file...")
+                    with open(f"./data/{id}.json", "rb+") as f:
+                        json_file = json.load(f)
+                        encrypted = json.dumps(json_file).encode()
+                        encrypted = StartupChecks.fernet.encrypt(encrypted).decode()
+                        write = {"encrypted": encrypted}
+                        f.seek(0)
+                        f.write(json.dumps(write, indent=4).encode())
+                        f.truncate()
+                        f.close()
+            else:
+                print("JSON file for user ID {} exists...".format(id))
