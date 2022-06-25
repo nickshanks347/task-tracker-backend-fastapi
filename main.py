f4e20eb21aef25a9475cfda2c800e2830f0cc3de
Nick <nick@tfsi.dev>
2022-06-25 02:25:07 +0100

    exit program if no config file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
-from apis import router as router
+try:
+    from fastapi import FastAPI
+    from apis import router as router
 
-app = FastAPI(title="Todo API", description="Todo API using FastAPI", version="0.2.0")
+    app = FastAPI(title="Todo API", description="Todo API using FastAPI", version="0.2.0")
 
-app.include_router(router, prefix="/api")
+    app.include_router(router, prefix="/api")
 
-if __name__ == "__main__":
-    import uvicorn
+    if __name__ == "__main__":
+        import uvicorn
 
-    uvicorn.run("main:app", reload=True)
+        uvicorn.run("main:app", reload=True)
+except FileNotFoundError:
+    print("Config file not found...")
+    print("Please ensure config.yaml exists in the data directory...")
+    exit(1)
