a128170932764d573b227fb1852ddefcf291711a
Nick <nick@tfsi.dev>
2022-06-27 01:49:01 +0000

    fix env variable int issue

--- core/config.py
+++ core/config.py
@@ -9,7 +9,7 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JSON_SECRET_KEY = os.getenv("JSON_SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
     ENABLE_REGISTRATIONS = os.getenv("ENABLE_REGISTRATIONS") == "1"
     ENCRYPT_JSON = os.getenv("ENCRYPT_JSON") == "1"
     RELOAD = os.getenv("RELOAD") == "1"
